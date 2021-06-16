cbfa6d4b56b4f77de96db361ee21f8dddb137b1e
Caroline Tan <carolinectan3@gmail.com>
2021-06-16 12:42:03 -0400

    Add sorted cargo list and total inventory methods

--- lib/train_yard.rb
+++ lib/train_yard.rb
@@ -21,4 +21,28 @@ class TrainYard
       train.cargo.keys.include?(car)
     end
   end
+
+  def sorted_cargo_list
+    @trains.flat_map do |train|
+      train.cargo.keys.map do |car|
+        car.type
+      end
+    end.uniq.sort
+ end
+
+ def total_inventory
+   hash = Hash.new(0)
+
+   @trains.each do |train|
+     train.cargo.each do |car, amt|
+       hash[car] += amt
+     end
+   end
+   hash
+ end
+
+
+  # Your TrainYard will also be able to identify `overflow_cars` A car is overflowing if it is carried by more
+  # than one train and there are more than ten cars.
+
 end
