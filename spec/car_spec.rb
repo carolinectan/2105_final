19ad3b2e1ea473e0b1a3d139bb228e63d2b9054a
Caroline Tan <carolinectan3@gmail.com>
2021-06-16 11:41:21 -0400

    Refactor

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,21 +2,23 @@ require 'rspec'
 require './lib/car'
 
 RSpec.describe Car do
-  before :each do
-    @car1 = Car.new({type: 'Mail', weight: 5})
-    @car2 = Car.new({type: 'Passengers', weight: 1})
-  end
+  describe 'iteration 1' do
+    before :each do
+      @car1 = Car.new({type: 'Mail', weight: 5})
+      @car2 = Car.new({type: 'Passengers', weight: 1})
+    end
 
-  it 'exists' do
-    expect(@car1).to be_a(Car)
-    expect(@car2).to be_a(Car)
-  end
+    it 'exists' do
+      expect(@car1).to be_a(Car)
+      expect(@car2).to be_a(Car)
+    end
 
-  it 'initializes with attributes' do
-    expect(@car1.type).to eq('Mail')
-    expect(@car1.weight).to eq(5)
+    it 'initializes with attributes' do
+      expect(@car1.type).to eq('Mail')
+      expect(@car1.weight).to eq(5)
 
-    expect(@car2.type).to eq('Passengers')
-    expect(@car2.weight).to eq(1)
+      expect(@car2.type).to eq('Passengers')
+      expect(@car2.weight).to eq(1)
+    end
   end
 end
